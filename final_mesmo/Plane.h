a43cfa6525a5385c0ad8f73a236bb2f5734b93a6
Pedro <pedrovargastannuri@gmail.com>
2023-06-06 09:58:48 -0300

    deleted useless files

--- final_mesmo/Plane.h
+++ final_mesmo/Plane.h
@@ -6,44 +6,45 @@
 #include "Vect.h"
 #include "Color.h"
 
-class Plane : public Object {
+class Plane : public Object
+{
 	Vect normal;
 	double distance;
 	Color color;
-	
-	public:
-	
-	Plane (Vect normalValue, double distanceValue, Color colorValue) {
+
+public:
+	Plane(Vect normalValue, double distanceValue, Color colorValue)
+	{
 		normal = normalValue;
 		distance = distanceValue;
 		color = colorValue;
 	}
 
-	
 	// method functions
-	Vect getPlaneNormal () { return normal; }
-	double getPlaneDistance () { return distance; }
-	virtual Color getColor () { return color; }
-	
-	virtual Vect getNormalAt(Vect point) {
+	Vect getPlaneNormal() { return normal; }
+	double getPlaneDistance() { return distance; }
+	virtual Color getColor() { return color; }
+
+	virtual Vect getNormalAt(Vect point)
+	{
 		return normal;
 	}
-	
-	virtual double findIntersection(Ray ray) {
+
+	virtual double findIntersection(Ray ray)
+	{
 		Vect ray_direction = ray.getRayDirection();
-		
+
 		double a = ray_direction.dotProduct(normal);
-		
+
 		// se o ray nao for paralelo ao plano
-		if(a != 0) {
+		if (a != 0)
+		{
 			double b = normal.dotProduct(ray.getRayOrigin().vectAdd(normal.vectMult(distance).negative()));
-			return -1*b/a;
+			return -1 * b / a;
 		}
 
 		return -1;
 	}
-	
 };
-
 
 #endif
